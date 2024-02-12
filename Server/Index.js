0c51ce848c93575ca9bb9da544b02cce8388e1c2
Nahidahmmed <nahidahmmed411@gmail.com>
2024-02-12 19:23:15 +0600

    set rout for detaile page

--- Server/Index.js
+++ Server/Index.js
@@ -52,6 +52,19 @@ async function run() {
         res.send(result);
       })
 
+      app.get('/classes/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await ClassesCollection.findOne(query);
+        res.send(result);
+    })
+      app.get('/instructors/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await InstructorsCollection.findOne(query);
+        res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
