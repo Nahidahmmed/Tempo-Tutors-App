1d0f94147c7604f4879010c4cadbcc72934838d2
Nahidahmmed <nahidahmmed411@gmail.com>
2024-03-02 19:28:27 +0600

    firebase auto import

--- Clinet/src/Providers/AuthProvider.jsx
+++ Clinet/src/Providers/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import React, { createContext, useEffect, useState } from 'react'
-import app from "../firebase/firebase.config"
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth"
+import app from '../firebase/firebase.config';
+
 export const AuthContext = createContext(null);
 export default function AuthProvider({children}) {
     const auth = getAuth(app)
