932b304daa431f2823026353d6f2e66dd10c3da1
Nahidahmmed <nahidahmmed411@gmail.com>
2024-03-02 19:48:54 +0600

    no ipmort

--- Clinet/src/Providers/AuthProvider.jsx
+++ Clinet/src/Providers/AuthProvider.jsx
@@ -1,7 +1,22 @@
 import React, { createContext, useEffect, useState } from 'react'
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth"
-import app from '../firebase/firebase.config';
+// import app from '../firebase/firebase.config';
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCo2X23vm2fRVt11m6FS8fv1smLKhVcCoU",
+  authDomain: "tempo-tutors-3d4b3.firebaseapp.com",
+  projectId: "tempo-tutors-3d4b3",
+  storageBucket: "tempo-tutors-3d4b3.appspot.com",
+  messagingSenderId: "447466441253",
+  appId: "1:447466441253:web:6d651469431ad464aa8dcc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 export const AuthContext = createContext(null);
 export default function AuthProvider({children}) {
